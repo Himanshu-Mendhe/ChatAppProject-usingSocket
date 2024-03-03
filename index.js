1dfb6ac7a1a190ae470c9157798037c73cd5cc39
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-03-03 13:04:53 +0530

    added codes

--- index.js
+++ index.js
@@ -9,17 +9,14 @@ const io = socketIo(server);
 io.on('connection', (socket) => {
     console.log('a user connected having socket id',socket.id);
 
-    socket.on('from_clint', () => {   // on listrn to the event
-        console.log('event comming from clint');  
+    socket.on('msg_send', (data) => {
+        console.log(data);
+
+        io.emit('msg_recived', data); //both/ everyoe could see it
+        //socket.broadcast.emit('msg_recived', data);// only receiver could see it
+        //socket.emit('msg_recived', data)// only sender could see it
     })
-
-    setInterval(() => {
-        socket.emit('from_server') //emit publishes the event
-    },2000
-    );
 })
-
-
 
 
 app.use('/',express.static(__dirname + '/public')); //deduct all static files like html static
--- public/script.js
+++ public/script.js
@@ -1,15 +1,17 @@
 var socket = io();
 
 let btn = document.getElementById('btn');
+let inputMsg = document.getElementById('newmsg');
+let msgList = document.getElementById('msgList');
+
 btn.onclick = function exec() {
-    socket.emit('from_clint')
+    socket.emit('msg_send', {
+        msg: inputMsg.value
+    });  
 }
 
-socket.on('from_server', () => {
-
-    console.log("collected new event from server")
-    const div = document.createElement('div');
-    div.innerText = 'New event from server';
-    console.log(div)
-    document.body.appendChild(div);
+socket.on('msg_recived', (data) => {
+    let listMsg = document.createElement('li');
+    listMsg.innerText = data.msg;
+    msgList.appendChild(listMsg);
 })
