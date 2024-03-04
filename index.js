d9add1ee3d5fac6f0fa9ad600e26492e75c225f9
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-03-04 11:11:15 +0530

    remove stat button adn initiated connection as soon as user joins

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const connect = require('./config/db-config');
+const ejs = require('ejs');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server); 
 
 io.on('connection', (socket) => {
-    console.log('a user connected having socket id',socket.id);
+
+    socket.on('join_room', (data) => {
+        socket.join(data.roomId);
+    })
 
     socket.on('msg_send', (data) => {
         console.log(data);
 
-        io.emit('msg_recived', data); //both/ everyoe could see it
+        io.to(data.roomId).emit('msg_recived', data); //both/ everyoe could see it
         //socket.broadcast.emit('msg_recived', data);// only receiver could see it
         //socket.emit('msg_recived', data)// only sender could see it
     })
 })
 
-
+app.set('view engine', 'ejs')
 app.use('/',express.static(__dirname + '/public')); //deduct all static files like html static
+app.get('/chat/:roomId', (req,res) => {
+    res.render('index', {
+        name: 'himanshu',
+        id: req.params.roomId
+    })
+})
 
-
-server.listen(3000, () => {
-    console.log(`server started on port 3000`)
+server.listen(3000, async() => {
+    console.log(`server started on port 3000`);
+    await connect();
+    console.log('mongod connected');
 })
